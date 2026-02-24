63aaa2595852002aff0b51a690cdbd19e892e979
mattimacca <mmaccaroni@frba.utn.edu.ar>
2026-02-23 21:06:48 -0300

    Finales Recientes

--- Finales/4-3-24.cpp
+++ Finales/4-3-24.cpp
@@ -9,9 +9,17 @@ struct Lenguaje{
     int otros;
 };
 
+struct NodoLenguajeNuevo{
+    char otro[50];
+    int cant;
+    NodoLenguajeNuevo* sig;
+};
+    
+
+
 struct NodoLenguaje{
     Lenguaje info;
-    NodoLenguaje* sig;
+    NodoLenguajeNuevo* nuevosLenguajes;  
 };
 
 struct Encuesta{ // Necesaria??, creo que no.
@@ -22,17 +30,21 @@ struct Encuesta{ // Necesaria??, creo que no.
 
 int unaEncuesta(int &act, int &inst,int &len, char* cual);
 
-void cargarMatriz(Lenguaje matriz[2][2], int act, int inst, int len, char* otro){
-    
+void cargarMatriz(Lenguaje matriz[3][3], int act, int inst, int len, char* otro){
+    for(int i=0; i<4; i++){
+        for(int j=0; j<4;j++){
+            
+        }
+    }
 }
 
 int main(){
     int act, inst, len;
     char otro[50];
 
-    Lenguaje encuestas[2][2];
+    Lenguaje encuestas[3][3];
 
     while(unaEncuesta(act, inst, len, otro) != 0){
-        cargarMatriz
+        //cargarMatriz();
     }
 }
